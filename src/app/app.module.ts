61d79cd3a12a36f2a2776a54638961dbde14e4af
towficaziz <tapolly01@gmail.com>
2023-04-10 22:56:40 +0200

    Outsourcing the Route Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,11 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes =[
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children:[
-      { path: ':id/:name', component: UserComponent }
-  ] },
-  { path: 'servers', component: ServersComponent, children:[
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
-  ] },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found', pathMatch:'full'}
-];
+
 
 @NgModule({
   declarations: [
@@ -43,7 +32,7 @@ const appRoutes: Routes =[
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
